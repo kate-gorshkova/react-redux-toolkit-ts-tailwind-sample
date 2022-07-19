e8bde2607cfdb31545202ea1b944a7f925efe887
Kate <katerina.gorshkova10@gmail.com>
2022-07-19 14:59:20 +0400

    [feat]: app completed

---
+++ src/pages/FavouritesPage.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useAppSelector } from '../hooks/redux'
+
+const FavouritesPage = () => {
+
+    const { favorites } = useAppSelector(state => state.github)
+
+    if (favorites.length === 0) return <p className='text-center'>No items...</p>
+
+    return (
+        <div className='flex justify-center pt-10 mx-auto h-screen w-screen'>
+            <ul className='list-none'>
+                {favorites.map(fav => (
+                    <li key={fav}>
+                        <a href={fav} target='_blank'>{fav}</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default FavouritesPage
